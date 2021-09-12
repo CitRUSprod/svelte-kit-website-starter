b2f7d57519b6a697917d3973386f882b686eb516
CitRUS <citrus4009@gmail.com>
2021-09-12 20:10:10 +0300

    feat(api): Add schemas for routes

--- services/api/src/routes/auth/login.ts
+++ services/api/src/routes/auth/login.ts
@@ -1,7 +1,6 @@
 import { FastifyPluginCallback } from "fastify"
 import { BadRequest } from "http-errors"
 import argon2 from "argon2"
-import { Payload } from "$/types"
 import { User, RefreshToken } from "$/db/entities"
 
 interface LoginData {
@@ -14,47 +13,56 @@ const route = ((app, opts, done) => {
     const refreshTokensRepository = app.orm.getRepository(RefreshToken)
 
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    app.post<{ Body: LoginData }>("/", async (req, reply) => {
-        const { email, password } = req.body
+    app.post<{ Body: LoginData }>("/", {
+        schema: {
+            body: {
+                type: "object",
+                properties: {
+                    email: { type: "string", minLength: 6, maxLength: 64 },
+                    password: { type: "string", minLength: 8 }
+                },
+                required: ["email", "password"]
+            }
+        },
+        async handler(req, reply) {
+            const { email, password } = req.body
 
-        const trimmedEmail = email.trim().toLowerCase()
+            const trimmedEmail = email.trim().toLowerCase()
 
-        const user = await usersRepository.findOne({ email: trimmedEmail })
+            const user = await usersRepository.findOne({ email: trimmedEmail })
 
-        if (!user) {
-            reply.send(new BadRequest("User with such email was not found"))
-            return
+            if (!user) {
+                reply.send(new BadRequest("User with such email was not found"))
+                return
+            }
+
+            const isCorrectPassword = await argon2.verify(user.password, password)
+
+            if (!isCorrectPassword) {
+                reply.send(new BadRequest("Incorrect password"))
+                return
+            }
+
+            const tokens = app.generateTokens({ id: user.id })
+
+            let refreshToken = await refreshTokensRepository.findOne({ user })
+
+            if (!refreshToken) {
+                refreshToken = refreshTokensRepository.create()
+            }
+
+            refreshToken.token = tokens.refresh
+            refreshToken.user = user
+            await refreshTokensRepository.save(refreshToken)
+
+            reply
+                .setCookie("refreshToken", tokens.refresh, {
+                    path: "/",
+                    maxAge: 30 * 24 * 60 * 60 * 1000,
+                    httpOnly: true
+                })
+                .send({ token: tokens.access })
         }
-
-        const isCorrectPassword = await argon2.verify(user.password, password)
-
-        if (!isCorrectPassword) {
-            reply.send(new BadRequest("Incorrect password"))
-            return
-        }
-
-        const payload: Payload = {
-            id: user.id
-        }
-        const tokens = app.generateTokens(payload)
-
-        let refreshToken = await refreshTokensRepository.findOne({ user })
-
-        if (!refreshToken) {
-            refreshToken = refreshTokensRepository.create()
-        }
-
-        refreshToken.token = tokens.refresh
-        refreshToken.user = user
-        await refreshTokensRepository.save(refreshToken)
-
-        reply
-            .setCookie("refreshToken", tokens.refresh, {
-                path: "/",
-                maxAge: 30 * 24 * 60 * 60 * 1000,
-                httpOnly: true
-            })
-            .send({ token: tokens.access })
     })
 
     done()
--- services/api/src/routes/auth/register.ts
+++ services/api/src/routes/auth/register.ts
@@ -13,37 +13,50 @@ const route = ((app, opts, done) => {
     const usersRepository = app.orm.getRepository(User)
 
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    app.post<{ Body: RegistrationData }>("/", async (req, reply) => {
-        const { email, username, password } = req.body
+    app.post<{ Body: RegistrationData }>("/", {
+        schema: {
+            body: {
+                type: "object",
+                properties: {
+                    email: { type: "string", minLength: 6, maxLength: 64 },
+                    username: { type: "string", minLength: 3, maxLength: 64 },
+                    password: { type: "string", minLength: 8 }
+                },
+                required: ["email", "username", "password"]
+            }
+        },
+        async handler(req, reply) {
+            const { email, username, password } = req.body
 
-        const trimmedEmail = email.trim().toLowerCase()
-        const trimmedUsername = username.trim()
+            const trimmedEmail = email.trim().toLowerCase()
+            const trimmedUsername = username.trim()
 
-        const userByEmail = await usersRepository.findOne({ email: trimmedEmail })
+            const userByEmail = await usersRepository.findOne({ email: trimmedEmail })
 
-        if (userByEmail) {
-            reply.send(new BadRequest("A user with this email already exists"))
-            return
+            if (userByEmail) {
+                reply.send(new BadRequest("A user with this email already exists"))
+                return
+            }
+
+            const userByUsername = await usersRepository.findOne({ username: trimmedUsername })
+
+            if (userByUsername) {
+                reply.send(new BadRequest("A user with this username already exists"))
+                return
+            }
+
+            const passwordHash = await argon2.hash(password)
+
+            const user = usersRepository.create({
+                email: trimmedEmail,
+                username: trimmedUsername,
+                password: passwordHash
+            })
+
+            await usersRepository.save(user)
+
+            reply.send()
         }
-
-        const userByUsername = await usersRepository.findOne({ username: trimmedUsername })
-
-        if (userByUsername) {
-            reply.send(new BadRequest("A user with this username already exists"))
-            return
-        }
-
-        const passwordHash = await argon2.hash(password)
-
-        const user = usersRepository.create({
-            email: trimmedEmail,
-            username: trimmedUsername,
-            password: passwordHash
-        })
-
-        await usersRepository.save(user)
-
-        reply.send()
     })
 
     done()
