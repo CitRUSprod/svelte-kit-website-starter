10fefa62aa994a2b2b83adacdeaae9e106526a81
CitRUS <citrus4009@gmail.com>
2024-01-25 21:49:24 +0300

    refactor(api): Replace prisma deprecated methods

--- apps/api/src/decorators/prisma.ts
+++ apps/api/src/decorators/prisma.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance, FastifyPluginCallback } from "fastify"
-import { PrismaClient, Prisma, Role } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 import * as constantsEnums from "@local/constants/enums"
 
 declare module "fastify" {
@@ -8,73 +8,26 @@ declare module "fastify" {
     }
 }
 
-const actionsWithOne: Array<Prisma.PrismaAction> = [
-    "findFirst",
-    "findUnique",
-    "create",
-    "update",
-    "upsert",
-    "delete"
-]
-const actionsWithMany: Array<Prisma.PrismaAction> = [
-    "findMany",
-    "createMany",
-    "updateMany",
-    "deleteMany"
-]
-
-function setAllPermissionsForAdmin(role?: Role) {
-    if (role?.id === 2) {
-        role.permissions = Object.values(constantsEnums.Permission)
-    }
-}
-
 export const prisma: FastifyPluginCallback = (app, options, done) => {
-    const client = new PrismaClient()
-
-    client.$use(async (params, next) => {
-        const result = await next(params)
-
-        if (params.model === Prisma.ModelName.Role) {
-            if (actionsWithOne.includes(params.action)) {
-                const role = result
-                setAllPermissionsForAdmin(role)
-            } else if (actionsWithMany.includes(params.action)) {
-                const roles = result
-
-                for (const role of roles) {
-                    setAllPermissionsForAdmin(role)
-                }
-            }
-        } else if (params.model === Prisma.ModelName.User) {
-            if (actionsWithOne.includes(params.action)) {
-                const user = result
-                setAllPermissionsForAdmin(user?.role)
-            } else if (actionsWithMany.includes(params.action)) {
-                const users = result
-
-                for (const user of users) {
-                    setAllPermissionsForAdmin(user?.role)
-                }
-            }
-        } else if (params.model === Prisma.ModelName.Post) {
-            if (actionsWithOne.includes(params.action)) {
-                const post = result
-                setAllPermissionsForAdmin(post?.author?.role)
-            } else if (actionsWithMany.includes(params.action)) {
-                const posts = result
-
-                for (const post of posts) {
-                    setAllPermissionsForAdmin(post?.author?.role)
+    const client = new PrismaClient().$extends({
+        result: {
+            role: {
+                permissions: {
+                    needs: { id: true, permissions: true },
+                    compute(role) {
+                        if (role.id === 2) {
+                            return Object.values(constantsEnums.Permission)
+                        } else {
+                            return role.permissions
+                        }
+                    }
                 }
             }
         }
-
-        return result
     })
 
     client.$connect().then(() => {
-        app.decorate<FastifyInstance["prisma"]>("prisma", client)
+        app.decorate<FastifyInstance["prisma"]>("prisma", client as any)
 
         done()
     })
