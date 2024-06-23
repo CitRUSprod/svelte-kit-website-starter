2f5c9b31613d9f7eaaa22bf858430bce62b25d97
CitRUS <citrus4009@gmail.com>
2024-06-24 01:19:34 +0400

    build(web): Add wsl build

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -3,11 +3,13 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import dynamicImport from "vite-plugin-dynamic-import"
 import { HstSvelte as hstSvelte } from "@histoire/plugin-svelte"
 
+const isWsl = "WSL_DISTRO_NAME" in process.env
+
 /** @type {import("vite").UserConfig} */
 const config = {
     server: {
         port: 6701,
-        host: true
+        host: !isWsl
     },
     plugins: [unocss(), sveltekit(), dynamicImport()],
     histoire: {
